734e6c86c35733218da42ef683a47bc5cdc09c8d
ree <naura.rara36@gmail.com>
2026-04-01 00:15:04 +0700

    TM4 dan TP5

---
+++ 04_Automata_dan_Table-driven_Construction/TM/TM4.js
@@ -0,0 +1,49 @@
+const editorElement = document.getElementById("editor-kecil");
+const charCountElement = document.getElementById("hf");
+const smallLetterElement = document.getElementById("hk");
+const hurufBesarBtn = document.getElementById("huruf-besar");
+const hurufKecilBtn = document.getElementById("huruf-kecil");
+
+// Hitung karakter & huruf kecil
+editorElement.addEventListener("input", (event) => {
+    const text = event.target.value;
+    charCountElement.textContent = text.length;
+
+    let jumlahKecil = 0;
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] >= 'a' && text[i] <= 'z') jumlahKecil++;
+    }
+    smallLetterElement.textContent = jumlahKecil;
+});
+
+// Tombol besar/kecil
+hurufBesarBtn.addEventListener("click", () => {
+    editorElement.value = editorElement.value.toUpperCase();
+});
+
+hurufKecilBtn.addEventListener("click", () => {
+    editorElement.value = editorElement.value.toLowerCase();
+});
+
+// Mode terang/gelap
+function modeTerang() {
+    document.body.classList.remove("dark-mode");
+    document.body.classList.add("light-mode");
+}
+
+function modeGelap() {
+    document.body.classList.remove("light-mode");
+    document.body.classList.add("dark-mode");
+}
+
+function setMode(mode) {
+  document.body.classList.remove("light-mode", "dark-mode", "sepia-mode");
+
+  if (mode === "light") {
+    document.body.classList.add("light-mode");
+  } else if (mode === "dark") {
+    document.body.classList.add("dark-mode");
+  } else if (mode === "sepia") {
+    document.body.classList.add("sepia-mode");
+  }
+}
---
+++ 04_Automata_dan_Table-driven_Construction/TM/TM_4.md
@@ -0,0 +1,33 @@
+## Tugas pendahuluan: GUI menggunakan HTML dan CSS.
+
+**Nama:** Naura Felicia Fatliaskamto
+
+**NIM:** 103122400046
+
+**Kelas:** SE-08-02
+
+## Soal
+
+Tambahkan mode sepia dengan ketentuan:
+
+Elemen	Warna
+Latar belakang	#F4ECD8
+Warna teks	#5B4636
+Biarkan form tetap warna putih.
+
+Ketentuan lainnya:
+
+Bagian mode-div harus menaungi tiga button: light, dark, dan sepia
+Bisa berpindah state secara mulus: sepia menghasilkan sepia-mode, dark menghasilkan dark-mode, dan light menghasilkan light-mode
+
+## Kode Sumber
+
+Tersedia di [TM4.html](./TM4.html) , [TM4.js](./TM4.js) dan , [TM4.css](./TM4.css)
+
+## Output
+
+![Mode sepia](image.png)
+
+## Deskripsi
+
+Fitur mode sepia ditambahkan sebagai alternatif tampilan selain mode terang dan gelap untuk meningkatkan kenyamanan visual pengguna. Mode ini menggunakan warna latar belakang #F4ECD8 dan warna teks #5B4636, sehingga memberikan kesan hangat dan nyaman di mata. Implementasi dilakukan dengan menambahkan kelas CSS sepia-mode serta mengatur perpindahan mode menggunakan JavaScript melalui manipulasi classList pada elemen <body>. Disediakan tiga tombol (light, dark, sepia) untuk mengubah tampilan.
